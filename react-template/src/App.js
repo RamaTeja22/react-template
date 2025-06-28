4c3107fd405d912caacf6ffee19b18d3b18b9446
Rama Teja <2ramateja@gmail.com>
2025-06-29 01:07:43 +0530

    boiler plate code

---
+++ react-template/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Interview Template</h1>
+      {/* Add your solution code here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ react-template/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css'; // Optional: Import global styles
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
